36ad1a654a55f9bc456ae341bec27b6ace2f662c
Sergei Shmanko <sergei.shmanko@rangeforce.com>
2021-07-29 14:27:32 +0300

    f

--- build.ps1
+++ build.ps1
@@ -14,5 +14,5 @@ packer build --force -var-file="$var_file" "$template_file"
 $endDTM = (Get-Date)
 Write-Host "[INFO]  - Elapsed Time: $(($endDTM-$startDTM).totalseconds) seconds" -ForegroundColor Yellow
 
-Convert-VHD -Path "D:\packer\output\Virtual Hard Disks\packer-vm.vhdx" -DestinationPath "D:\hv-packer\output\Virtual Hard Disks\packer-vm.vhd" -VHDType Fixed
+Convert-VHD -Path "D:\packer\output\Virtual Hard Disks\packer-vm.vhdx" -DestinationPath "D:\packer\output\Virtual Hard Disks\packer-vm.vhd" -VHDType Fixed
 (Get-VHD 'D:\packer\output\Virtual Hard Disks\packer-vm.vhd').FileSize | Out-File -FilePath D:\disksize
